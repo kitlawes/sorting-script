92b98c6067a14f8070a6d34a34c1be6c3c5208e8
Kit Lawes <kitlawes@hotmail.com>
2017-05-13 14:47:11 +0100

    Support reading and writing files

--- src/script.php
+++ src/script.php
@@ -1,5 +1,13 @@
 <?php
 
-echo True ? "yes" : "no";
+$inputfilepath = "../resources/random_100.txt";
+$inputfile = fopen($inputfilepath, "r") or die("Unable to open " . $inputfilepath);
+$contents = fread($inputfile, filesize($inputfilepath));
+fclose($inputfile);
+
+$outputfilepath = "../resources/output.txt";
+$outputfile = fopen($outputfilepath, "w") or die("Unable to open " . $inputfilepath);
+fwrite($outputfile, $contents);
+fclose($outputfile);
 
 ?>

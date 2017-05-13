16034d91aefe0c848fa8671cf3c4756013153234
Kit Lawes <kitlawes@hotmail.com>
2017-05-13 15:09:41 +0100

    Implement the gnome sort sorting algorithm

--- src/script.php
+++ src/script.php
@@ -2,12 +2,33 @@
 
 $inputfilepath = "../resources/random_100.txt";
 $inputfile = fopen($inputfilepath, "r") or die("Unable to open " . $inputfilepath);
-$contents = fread($inputfile, filesize($inputfilepath));
+$inputfilecontents = fread($inputfile, filesize($inputfilepath));
 fclose($inputfile);
 
+$numbers = explode(",", $inputfilecontents);
+$outputfilecontents = "";
+$i = 0;
+while ($i < sizeof($numbers)) {
+	if ($i == 0 || $numbers[$i - 1] <= $numbers[$i]) {
+		$i++;
+	} else {
+		$previousnumber = $numbers[$i - 1];
+		$numbers[$i - 1] = $numbers[$i];
+		$numbers[$i] = $previousnumber;
+		$i--;
+    }
+}
+for ($i = 0; $i < sizeof($numbers); $i++) {
+	if ($i == 0) {
+		$outputfilecontents .= $numbers[$i];
+    } else {
+		$outputfilecontents .= "," . $numbers[$i];
+    }
+}
+
 $outputfilepath = "../resources/output.txt";
-$outputfile = fopen($outputfilepath, "w") or die("Unable to open " . $inputfilepath);
-fwrite($outputfile, $contents);
+$outputfile = fopen($outputfilepath, "w") or die("Unable to open " . $outputfilepath);
+fwrite($outputfile, $outputfilecontents);
 fclose($outputfile);
 
 ?>
